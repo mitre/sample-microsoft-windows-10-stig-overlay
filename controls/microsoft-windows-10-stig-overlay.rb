cd3043a0ced4b7abfd4bb5790d880a6e335174c4
naejohnson <naejohnson@mitre.org>
2020-12-29 16:21:48 -0500

    Initial commit

---
+++ controls/microsoft-windows-10-stig-overlay.rb
@@ -0,0 +1,324 @@
+
+include_controls 'microsoft-windows-10-stig-baseline' do
+
+    control 'V-63321' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63325' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63335' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63339' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63341' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63343' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63345' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63347' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63369' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63375' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63431' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63435' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63445' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63447' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63449' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63451' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63453' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63457' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63459' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63463' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63467' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63469' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63471' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63473' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63479' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63481' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63483' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63487' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63491' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63499' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63503' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63507' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63513' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63515' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63517' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63519' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63523' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63527' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63533' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63537' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63541' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63579' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63583' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63587' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63589' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63635' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63645' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63649' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63669' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63675' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63681' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63689' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63729' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63733' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63737' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63741' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63817' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63821' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63829' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63879' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-63927' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-68817' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-68819' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-68845' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-68849' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-71759' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-71761' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-74409' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-74411' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-74721' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-75027' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-77095' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-94719' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-99541' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-99543' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-99545' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-99547' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-99549' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-99551' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+    control 'V-99553' do
+        impact 0.0
+        desc 'caveat', 'This is Not Applicable since the related security control is not included in ***SPONSOR*** policy'
+    end
+end
